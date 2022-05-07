c75da3fff393a5e6276aa676968395786d338dd4
guilalf <guilherme.projetoismart@gmail.com>
2022-05-07 11:41:31 -0300

    Little import remove

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:calculadora_bhaskara/ui/pages/home_page.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MaterialApp(
